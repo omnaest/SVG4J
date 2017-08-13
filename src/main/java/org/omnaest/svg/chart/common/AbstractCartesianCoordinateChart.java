177e2064a3688bc205058e4cf7eec33f4ae74ae1
omnaest <awonderland6@googlemail.com>
2017-08-13 16:46:47 +0200

    refactored AbstractCartesianCoordinateChart

--- src/main/java/org/omnaest/svg/chart/common/AbstractCartesianCoordinateChart.java
+++ src/main/java/org/omnaest/svg/chart/common/AbstractCartesianCoordinateChart.java
@@ -1,3 +1,21 @@
+/*
+
+	Copyright 2017 Danny Kunz
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+		http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+
+
+*/
 package org.omnaest.svg.chart.common;
 
 import org.omnaest.svg.elements.SVGLine;
@@ -10,10 +28,11 @@ public abstract class AbstractCartesianCoordinateChart extends AbstractChart
 		super(width, height);
 	}
 
+	@Override
 	protected void renderVerticalAxis()
 	{
 		//
-		int padding = calculatePadding();
+		int padding = this.calculatePadding();
 		this.drawer.add(new SVGLine(padding, padding, padding, this.height - padding)	.setStrokeColor("gray")
 																						.setStrokeWidth(this.pixelFactor / 2));
 
@@ -22,14 +41,14 @@ public abstract class AbstractCartesianCoordinateChart extends AbstractChart
 		for (int ii = 0; ii < size; ii++)
 		{
 			//stroke
-			int y = calculateVerticalPosition(ii);
-			this.drawer.add(new SVGLine(padding - 2 * pixelFactor, y, padding + 2 * pixelFactor, y)	.setStrokeWidth(this.pixelFactor)
-																									.setStrokeColor("black"));
+			int y = this.calculateVerticalPosition(ii);
+			this.drawer.add(new SVGLine(padding - 2 * this.pixelFactor, y, padding + 2 * this.pixelFactor, y)	.setStrokeWidth(this.pixelFactor)
+																												.setStrokeColor("black"));
 
 			//text
 			IdAndLabel idAndLabel = this.verticalAxisValues.get(ii);
 			String label = idAndLabel.getLabel();
-			int fontSize = SVGText.DEFAULT_FONTSIZE * pixelFactor / 2;
+			int fontSize = SVGText.DEFAULT_FONTSIZE * this.pixelFactor / 2;
 			this.drawer.add(new SVGText(0, y + fontSize / 3, label)	.setColor("black")
 																	.setFontSize(fontSize));
 		}
@@ -40,7 +59,7 @@ public abstract class AbstractCartesianCoordinateChart extends AbstractChart
 		int padding = this.calculatePadding();
 		int verticalAxisSize = this.verticalAxisValues.size();
 		int chartAreaHeight = this.height - 2 * padding;
-		int spanHeight = chartAreaHeight / (verticalAxisSize - 1);
+		int spanHeight = verticalAxisSize > 0 ? chartAreaHeight / (verticalAxisSize - 1) : chartAreaHeight;
 		int y = this.height - padding - ii * spanHeight;
 		return y;
 	}
@@ -50,6 +69,7 @@ public abstract class AbstractCartesianCoordinateChart extends AbstractChart
 		return this.pixelFactor * 10;
 	}
 
+	@Override
 	protected void renderHorizontalAxis()
 	{
 		//
@@ -62,15 +82,15 @@ public abstract class AbstractCartesianCoordinateChart extends AbstractChart
 		for (int ii = 0; ii < size; ii++)
 		{
 			//stroke
-			int x = calculateHorizontalPosition(ii);
-			this.drawer.add(new SVGLine(x, this.height - padding + 2 * pixelFactor, x, this.height - padding - 2 * pixelFactor)
-																																.setStrokeWidth(this.pixelFactor)
-																																.setStrokeColor("black"));
+			int x = this.calculateHorizontalPosition(ii);
+			this.drawer.add(new SVGLine(x, this.height - padding + 2 * this.pixelFactor, x,
+										this.height - padding - 2 * this.pixelFactor)	.setStrokeWidth(this.pixelFactor)
+																						.setStrokeColor("black"));
 
 			//text
 			IdAndLabel idAndLabel = this.horizontalAxisValues.get(ii);
 			String label = idAndLabel.getLabel();
-			int fontSize = SVGText.DEFAULT_FONTSIZE * pixelFactor / 2;
+			int fontSize = SVGText.DEFAULT_FONTSIZE * this.pixelFactor / 2;
 			this.drawer.add(new SVGText(x - fontSize / 3, this.height, label)	.setColor("black")
 																				.setFontSize(fontSize)
 																				.setRotation(this.horizontalAxisOptions.getRotation()));
